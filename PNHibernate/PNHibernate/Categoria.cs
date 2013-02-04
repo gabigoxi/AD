363a7da54fdf3160cdff40333e1595b20597b23c
gabriel <gabriel9119@gmail.com>
2013-02-04 09:46:07 +0100

    Añadiendo PNHibernate

---
+++ PNHibernate/PNHibernate/Categoria.cs
@@ -0,0 +1,18 @@
+using System;
+using Gtk;
+
+namespace PNHibernate
+{
+		public class Categoria {
+		
+		public long Id {get; set;}
+		public string Nombre {get; set;}
+		
+		//private string nombre;
+		//private string Nombre {
+		//	get {return nombre;}
+		//	set {nombre = ValueType;}
+	
+		}
+	}
+
